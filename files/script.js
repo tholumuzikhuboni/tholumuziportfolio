62bdbdd846468d20c0a998d1e35c8b28d23756e9
Tholumuzi Khuboni <you@example.contact@tholumuzi.co.za>
2024-12-11 12:32:39 +0200

    Your commit message

--- files/script.js
+++ files/script.js
@@ -1,11 +1,7 @@
 'use strict';
 
-
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
